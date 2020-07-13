37219faf48fa6af033519cfbec3a9878363fa6c8
DeltaUser <DeltaUser@users.noreply.github.com>
2020-07-13 12:45:34 -0700

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -34,8 +34,8 @@ app.use(bodyParser.json());
         res.send(stats);
     });
     app.get('/api/stats', (req, res) => {
-        console.log(JSON.stringify(stats).replace(/"/g, '\"'));
-        if(req.useragent.isBot) return res.send(html('Statistics', 'https://macrot.herokuapp.com/api/stats', null, JSON.stringify(stats).replace(/"/g, '\"')));
+        console.log(JSON.stringify(stats).replace(/"/g, '\\"'));
+        if(req.useragent.isBot) return res.send(html('Statistics', 'https://macrot.herokuapp.com/api/stats', null, JSON.stringify(stats).replace(/"/g, '\\"')));
         res.send(stats);
     });
 })();
