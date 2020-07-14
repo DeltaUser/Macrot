18bb0049ee7392e6f2be5f6e306b44bc88e645f6
DeltaUser <DeltaUser@users.noreply.github.com>
2020-07-13 22:34:46 -0700

    Dashboard.

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-$(document).ready(() => {
+$(document).ready(async () => {
+    const status = await (await fetch('http://macrot.herokuapp.com/api/status')).json();
+    if(!status.dashboard) {
+        $('[id="statusMessage"]')[0].innerText = 'Dashboard is disabled.';
+        $('[id="dashboard"]')[0].disabled = true;
+        $('[id="dashboard"]').css('background-color', 'gray');
+        $('[id="dashboard"]').css('color', 'white');
+    }
     Particles.init({
         selector: '.background',
         color: 'black'
+    });
+    $('[id="dashboard"]').click(() => {
+        window.location = '/src/dashboard';
     });
 });
